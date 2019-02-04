017c9e22f8ba8ed6dc48ac89abed5ad03b3776d6
Yojan Pardo <Yojan.Pardo@gmail.com>
2019-02-04 16:37:59 -0500

    agrego el modelo Audit

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Audit(models.Model):
+	"""este es el modelo que almacenará los datos mas generales de la auditoria"""
+	auditor_name = models.CharField(max_length=50)
+	audited_name = models.CharField(max_length=50)
+	audited_job = models.CharField(max_length=255)
+	process_to_be_audited = models.CharField(max_length=100)
+	audit_scope = models.CharField(max_length=255)
+	audited_phone_number = models.CharField(max_length=15)
+	date = models.DateField()
+
+	#metadata
+
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+	
