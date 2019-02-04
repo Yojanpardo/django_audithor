26d7f8a09402d7cf4bfed088eeefe25a8f1aaffa
Yojan Pardo <Yojan.Pardo@gmail.com>
2019-02-04 17:12:28 -0500

    template home lista

--- audithor/urls.py
+++ audithor/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='base.html'), name='home'),
+    path('',TemplateView.as_view(template_name='home.html'), name='home'),
 ]
