6937d8b221cb231462679238cf9a5b372ead7f0a
Yojan Pardo <Yojan.Pardo@gmail.com>
2019-02-09 11:46:23 -0500

    agrego los ajustes para utilizar un login personalizado

--- audithor/settings.py
+++ audithor/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'audit',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
